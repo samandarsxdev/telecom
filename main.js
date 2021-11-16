b6d910c4c91ec9a65a5a690b71382b1a71e178f4
samandarsxdev <93664571+samandarsxdev@users.noreply.github.com>
2021-11-16 05:58:30 -0500

    add

--- main.js
+++ main.js
@@ -15,8 +15,16 @@
 // var kun=sana.getDate();
 // var haftaKuni=sana.getDay();
 // document.write(yil+"/"+haftaKuni+"/"+kun);
+//round funksiyani vazifasi berilgan butun bolmagan sonni butun qilib yaxlitlab beradi
 
-
+// var round=Math.round(8.5);
+// var pow=Math.pow(2, 5);
+// var ceil=Math.ceil(4.5);
+// var floor=Math.floor(4.9);
+// var min=Math.min(2,31,23,32,1.1);
+// var max=Math.max(2,3,4,5,6,6,7);
+// var trunc=Math.trunc(6.7);
+// document.write(trunc);
 
 
 
